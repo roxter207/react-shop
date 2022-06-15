2d31ffbad69da2ff963b6fdc8416a108da3034e5
roxter207 <66411952+roxter207@users.noreply.github.com>
2022-06-15 20:29:12 +0530

    Updates

---
+++ precache-manifest.6b85d8624ea687fcd671de50e4b00930.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6baef2620b78e469dd92513bd8f294f",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "2e97846b1ca9db25a853",
+    "url": "/react-shop/static/js/2.010e5e6a.chunk.js"
+  },
+  {
+    "revision": "2b1f621b3752c7e5f115",
+    "url": "/react-shop/static/js/main.c49f153a.chunk.js"
+  },
+  {
+    "revision": "75bd495123a48ea82bb9",
+    "url": "/react-shop/static/js/runtime~main.0bd8b961.js"
+  }
+]);
